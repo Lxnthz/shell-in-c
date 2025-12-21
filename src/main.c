2aa28e9e6e6bbda3fc747055f7f5e5963e172d88
Lxnthz <byanfidry@gmail.com>
2025-12-22 01:03:48 +0700

    stage 2

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  char command[256];
+
+  while (1) {
+    printf("$ ");
+
+    // Read user input
+    if (fgets(command, sizeof(command), stdin) == NULL) {
+      break;
+    }
+
+    // Remove trailing newline character
+    command[strcspn(command, "\n")] = '\0';
+
+    if (strcmp(command, "exit") == 0) {
+      break;
+    }
+  }
 
   return 0;
 }
