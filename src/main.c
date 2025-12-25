0f8dc0824883375e5ef9cb41abc384fbbbe69db7
Lxnthz <byanfidry@gmail.com>
2025-12-25 19:30:09 +0700

    stage 26 fix 8

--- src/main.c
+++ src/main.c
@@ -80,8 +80,8 @@ char *command_generator(const char *text, int state) {
           snprintf(fpath, sizeof(fpath), "%s/%s", dir, dp->d_name);
           if (access(fpath, X_OK) == 0) {
             // Append a single trailing space to the completion
-            char *completion = malloc(strlen(dp->d_name) + 2);
-            sprintf(completion, "%s", dp->d_name);
+            char *completion = malloc(strlen(dp->d_name) + 1);
+            sprintf(completion, "%s ", dp->d_name);
             return completion;
           }
         }
